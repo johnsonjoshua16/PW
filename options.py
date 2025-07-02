039f1702ccbe7d3d93b0e601c3614c643fbfdffd
Joshua Johnson <trillions22@hotmail.com>
2025-07-02 21:36:10 +0100

    Add files via upload

---
+++ options.py
@@ -0,0 +1,7 @@
+options = ["1. I want to know more about your products.",
+           "2. I need assistance with my order.",
+           "3. I have a question about your services.",
+           "4. I want to provide feedback.",
+           "5. I would like to order.",
+           "6. Contact us or exit.",
+           "7. How was your day?"]
